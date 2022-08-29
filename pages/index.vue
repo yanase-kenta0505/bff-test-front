a45c7b6e01b3141b89f23ca4d45d85ee6764008f
yanasekenta <ynsknt0505@gmail.com>
2022-08-29 11:59:36 +0900

    フロントの環境を構築

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+
+<script setup>
+const todos = ref([])
+
+const todoData = await useFetch(() => 'http://localhost:1337/api/todos/')
+try {
+  console.log(todoData.data.value.data)
+  todoData.data.value.data.forEach(data => console.log(data))
+  todos.value = todoData.data.value.data
+} catch (error) {
+  console.log(error)
+}
+
+</script>
+
+
+<template>
+  <div>
+    <ul>
+      <li v-for="(todo,index) in todos">{{todo.todoname}}</li>
+    </ul>
+  </div>
+</template>
